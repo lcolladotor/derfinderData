65c4c30759ba390cf7a3a2679e824062f72a6563
lcolladotor <lcollado@jhsph.edu>
2014-03-12 00:24:35 -0400

    Main TopHat output files

---
+++ extdata/thout/.sample14.th.sh
@@ -0,0 +1,11 @@
+#!/bin/bash	
+echo "**** Job starts ****"
+date
+
+# run tophat
+# defaults for polyester::simulate_experiment() are 100 bp paired reads from 250bp fragments with 25bp sd
+tophat -p 4 --transcriptome-index=/amber2/scratch/jleek/iGenomes-index/Homo_sapiens/UCSC/hg19/Annotation/Transcriptome/known --mate-inner-dis=50 --mate-std-dev=25 -o sample14 /amber2/scratch/jleek/iGenomes-index/Homo_sapiens/UCSC/hg19/Sequence/Bowtie2Index/genome /dcs01/lieber/ajaffe/Brain/derRuns/derfinderData/extdata/simulatedFasta/sample_14_1.fasta.gz /dcs01/lieber/ajaffe/Brain/derRuns/derfinderData/extdata/simulatedFasta/sample_14_2.fasta.gz
+
+mv /dcs01/lieber/ajaffe/Brain/derRuns/derfinderData/extdata/thout/sample14.* /dcs01/lieber/ajaffe/Brain/derRuns/derfinderData/extdata/thout/sample14/
+echo "**** Job ends ****"
+date
